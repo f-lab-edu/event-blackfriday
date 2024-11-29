8b92c9c154e5b7e29bedf6aa8f9a6112ea8d05cf
Jaeyeon Cho <cjyeon1022@gmail.com>
2024-11-29 15:22:01 +0900

    fix: deploy.sh 수정

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+DOCKER_COMPOSE="${DOCKER_CONFIG}/cli-plugins/docker-compose"
+
 CURRENT_PORT1=$(docker port blackfriday-app1 8080/tcp | cut -d ':' -f2 || echo "8080")
 CURRENT_PORT2=$(docker port blackfriday-app2 8080/tcp | cut -d ':' -f2 || echo "8081")
 
@@ -20,9 +23,9 @@ fi
 echo "New ports - APP1: $APP1_PORT, APP2: $APP2_PORT"
 
 echo "Deploying app1..."
-docker-compose up -d --no-deps app1
+$DOCKER_COMPOSE up -d --no-deps app1
 
 echo "Deploying app2..."
-docker-compose up -d --no-deps app2
+$DOCKER_COMPOSE up -d --no-deps app2
 
 echo "Deployment completed"
