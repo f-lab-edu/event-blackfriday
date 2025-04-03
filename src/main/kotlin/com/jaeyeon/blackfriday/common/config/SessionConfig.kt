be50fe45348203620f66c8df2186940a2be03c24
Jaeyeon Cho <cjyeon1022@gmail.com>
2025-04-03 12:48:20 +0900

    fix: 세션 설정 검증에 대한 로그 추가

--- src/main/kotlin/com/jaeyeon/blackfriday/common/config/SessionConfig.kt
+++ src/main/kotlin/com/jaeyeon/blackfriday/common/config/SessionConfig.kt
@@ -1,5 +1,7 @@
 package com.jaeyeon.blackfriday.common.config
 
+import jakarta.annotation.PostConstruct
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.session.web.http.HeaderHttpSessionIdResolver
@@ -7,9 +9,16 @@ import org.springframework.session.web.http.HttpSessionIdResolver
 
 @Configuration
 class SessionConfig {
+    private val log = LoggerFactory.getLogger(SessionConfig::class.java)
+
+    @PostConstruct
+    fun init() {
+        log.info("SessionConfig initialized with X-Auth-Token resolver")
+    }
 
     @Bean
     fun httpSessionIdResolver(): HttpSessionIdResolver {
+        log.info("Creating HeaderHttpSessionIdResolver.xAuthToken()")
         return HeaderHttpSessionIdResolver.xAuthToken()
     }
 }
